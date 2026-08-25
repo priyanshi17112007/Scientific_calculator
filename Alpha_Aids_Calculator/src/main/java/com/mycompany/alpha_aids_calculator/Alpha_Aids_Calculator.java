309d88b4df6c470b870132c651a5042116286e8e
Priyanshi Sharma <erpriyanshisharma15@gmail.com>
2026-08-25 12:56:18 +0530

    Add files via upload

---
+++ Alpha_Aids_Calculator/src/main/java/com/mycompany/alpha_aids_calculator/Alpha_Aids_Calculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.alpha_aids_calculator;
+
+/**
+ *
+ * @author tech
+ */
+public class Alpha_Aids_Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
